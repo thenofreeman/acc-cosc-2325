d5f80b2729726f8ca0859aee623ede1174b75c16
nolan freeman abbott <nolan@freemanabbott.com>
2022-09-15 13:03:23 -0500

    add labs

---
+++ lab-1/Fade_Without_Delay.ino
@@ -0,0 +1,46 @@
+/*
+  Fade
+
+  This example shows how to fade an LED on pin 9 using the analogWrite()
+  function.
+
+  The analogWrite() function uses PWM, so if you want to change the pin you're
+  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
+  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
+*/
+
+int led = 13;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+const int interval = 30; // millisecond interval delay per change in brightness
+unsigned long prev = 0; // the previous millisecond count
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 13 to be an output:
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  unsigned long curr = millis(); // the current millisecond count
+
+  if (curr - prev >= interval) {
+    prev = curr;
+    
+    // set the brightness of pin 13:
+    analogWrite(led, brightness);
+
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+  }
+}
